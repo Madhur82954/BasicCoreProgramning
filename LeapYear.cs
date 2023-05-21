5eaca93429e84eecafafec3733ad6e371907587a
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-21 11:52:59 +0530

    [Madhur] - Leap Year Added

---
+++ LeapYear.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCoreProgramning
+{
+    internal class LeapYear
+    {
+        public void checkleapyear()
+        {
+            Console.WriteLine("Enter Year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year%4==0)
+            {
+                Console.WriteLine("it is a Leap year");
+            }
+            else
+            {
+                Console.WriteLine("it is not a leap year");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,20 @@ namespace BasicCoreProgramning
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter option \n1) Flip Coin\n2) Leap Year");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
                     FlipCoin flip = new FlipCoin();
                     flip.checkHeadTail();
+                    break;
+
+                case 2:
+                    LeapYear leap = new LeapYear();
+                    leap.checkleapyear();
+                    break;
+            }
         }
     }
 }
